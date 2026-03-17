e33d969a9ab88b764742458a2af7f8f5d7ae671e
Chethan BN <bnchethan0708@gmail.com>
2026-03-17 19:48:01 +0530

    test npm test

---
+++ smartci/workspace/job-1/smartci/workspace/job-1/smartci/scheduler/jobScheduler.js
@@ -0,0 +1,56 @@
+const jobs = require("../store/jobStore");
+const runPipeline = require("../pipeline/pipelineManager");
+
+const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+
+let jobId = 1;
+
+function scheduleJob(repo, branch, commit) {
+
+  const id = jobId++;
+
+  const job = {
+    id,
+    repo,
+    branch,
+    commit,
+    status: "QUEUED",
+    stages: []
+  };
+
+  jobs.push(job);
+
+  console.log("Job scheduled:", id);
+
+  const repoUrl = `https://github.com/Prabhanjan-31/${repo}.git`;
+const workspace = path.join(__dirname, "..", "workspace", `job-${id}`);
+
+if (fs.existsSync(workspace)) {
+  fs.rmSync(workspace, { recursive: true, force: true });
+}
+
+fs.mkdirSync(workspace, { recursive: true });
+
+console.log("Cloning repository...");
+
+exec(`git clone ${repoUrl} .`, { cwd: workspace }, (err) => {
+
+  if (err) {
+    console.log("Clone failed:", err);
+    job.status = "FAILED";
+    return;
+  }
+
+  console.log("Repository cloned");
+
+  runPipeline(job, workspace);
+
+});
+
+  return job;
+}
+
+module.exports = scheduleJob;
