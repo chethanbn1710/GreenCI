13695280e49dd7d0c4dd368518230002ea532b49
Chethan BN <bnchethan0708@gmail.com>
2026-03-26 20:17:32 +0530

    test pipeline

--- smartci/pipeline/pipelineManager.js
+++ smartci/pipeline/pipelineManager.js
@@ -1,17 +1,65 @@
+const fs = require("fs");
+const path = require("path");
+
+function parseSmartCI(repoPath) {
+  const filePath = path.join(repoPath, ".smartci.yml");
+
+  if (!fs.existsSync(filePath)) {
+    console.log("No .smartci.yml found");
+    return [];
+  }
+
+  const content = fs.readFileSync(filePath, "utf-8");
+
+  const commands = [];
+  const lines = content.split("\n");
+
+  let insideScript = false;
+
+  lines.forEach(line => {
+    line = line.trim();
+
+    if (line.startsWith("script:")) {
+      insideScript = true;
+      return;
+    }
+
+    if (insideScript && line.startsWith("-")) {
+      commands.push(line.replace("-", "").trim());
+    }
+
+    // stop if next section starts
+    if (insideScript && !line.startsWith("-") && line !== "") {
+      insideScript = false;
+    }
+  });
+
+  return commands;
+}
+
 const { exec } = require("child_process");
 
-const stagesTemplate = [
-  "npm install",
-  "cd backend && npm install",
-  "cd backend && npm test"
-];
+// const stagesTemplate = [
+//   "npm install",
+//   "cd backend && npm install",
+//   "cd backend && npm test"
+// ];
 
 function runPipeline(job, repoPath) {
 
   job.status = "IN_PROGRESS";
 
-  job.stages = stagesTemplate.map(cmd => ({
-    name: cmd,
+  const commands = parseSmartCI(repoPath);
+
+  // fallback if file missing
+  if (commands.length === 0) {
+    console.log("Using default stages");
+    commands.push("npm install");
+  }
+
+  job.stages = commands.map((cmd, index) => ({
+    name: `Stage ${index + 1}`,
+    command: cmd,
     status: "PENDING"
   }));
 
@@ -19,6 +67,9 @@ function runPipeline(job, repoPath) {
 }
 
 function runStages(job, repoPath, index) {
+
+  console.log(`Running: ${stage.command}`);
+  console.log("Stages:", job.stages);
 
   if (index >= job.stages.length) {
     job.status = "COMPLETED";
@@ -30,7 +81,7 @@ function runStages(job, repoPath, index) {
 
   stage.status = "RUNNING";
 
-  exec(stage.name, { cwd: repoPath }, (err, stdout, stderr) => {
+  exec(stage.command, { cwd: repoPath }, (err, stdout, stderr) => {
 
     if (err) {
       stage.status = "FAILED";
