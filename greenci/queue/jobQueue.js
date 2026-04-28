bdfe66f7212adba074b31c697d68562bf53a5f40
Chethan BN <bnchethan0708@gmail.com>
2026-04-29 01:00:00 +0530

    db verification

--- greenci/queue/jobQueue.js
+++ greenci/queue/jobQueue.js
@@ -2,7 +2,7 @@ const jobQueue = [];
 
 function addJob(job) {
   jobQueue.push(job);
-  console.log("Job added to queue:", job._id);
+  console.log("Job added to queue:", job._id.toString());
 }
 
 function getQueue() {
