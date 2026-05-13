4d3d787cf6e58b49a91e2eee9889055a99e25b5e
Chethan BN <bnchethan0708@gmail.com>
2026-05-13 10:40:08 +0530

    simulation path correction

--- simulator/repoProfiles.js
+++ simulator/repoProfiles.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     name: "GreenDash_UI",
-    path: "../Jenkins_TargetRepos/GreenDash_UI",
+    path: "../../Jenkins_TargetRepos/GreenDash_UI",
     branches: ["main", "frontend"],
     files: ["app.js", "README.md"],
     frequency: 5000,
@@ -10,7 +10,7 @@ module.exports = [
 
   {
     name: "TaskForge_API",
-    path: "../Jenkins_TargetRepos/TaskForge_API",
+    path: "../../Jenkins_TargetRepos/TaskForge_API",
     branches: ["main", "api-backend"],
     files: ["app.py", "README.md"],
     frequency: 9000,
@@ -19,7 +19,7 @@ module.exports = [
 
   {
     name: "VisionTrain_AI",
-    path: "../Jenkins_TargetRepos/VisionTrain_AI",
+    path: "../../Jenkins_TargetRepos/VisionTrain_AI",
     branches: ["main", "ai-training"],
     files: ["train.py", "README.md"],
     frequency: 14000,
@@ -28,7 +28,7 @@ module.exports = [
 
   {
     name: "FastCompute_Core",
-    path: "../Jenkins_TargetRepos/FastCompute_Core",
+    path: "../../Jenkins_TargetRepos/FastCompute_Core",
     branches: ["main", "compute-core"],
     files: ["main.cpp", "README.md"],
     frequency: 11000,
